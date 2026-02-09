448abdde625ba5949315532ab01f421224145c86
dmitry-dmitry7 <nova_dima@ukr.net>
2026-02-09 02:32:29 +0000

    Deploying to gh-pages from @ dmitry-dmitry7/goit-js-hw-10@a83b5cc4d29a362743dff626d4a9fbf34a2c8f29 🚀

---
+++ 1-timer.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import"./assets/vendor-BfapnX2P.js";let t=document.querySelector("button");t.disabled=!0;console.log(t.disabled);
+//# sourceMappingURL=1-timer.js.map
