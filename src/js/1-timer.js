a83b5cc4d29a362743dff626d4a9fbf34a2c8f29
Dmytro <nova_dima@ukr.net>
2026-02-09 04:32:08 +0200

    add first part hw

---
+++ src/js/1-timer.js
@@ -0,0 +1,19 @@
+// Описаний в документації
+import flatpickr from "flatpickr";
+// Додатковий імпорт стилів
+import "flatpickr/dist/flatpickr.min.css";
+
+let button = document.querySelector('button');
+
+button.disabled = true;
+console.log(button.disabled);
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
